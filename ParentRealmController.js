0414bb9a4a197a1477087aad22bfd4be33786713
Tobias Trinks <tobias.trinks@code.berlin>
2018-01-24 16:04:58 +0100

    added menu functionality

--- ParentRealmController.js
+++ ParentRealmController.js
@@ -5,6 +5,9 @@ const OrderItem = require('./models/OrderItem.js');
 const Account = require('./models/Account');
 const Restaurant = require('./models/Restaurant.js');
 const VoiceDevice = require('./models/VoiceDevice.js');
+const Drink = require('./models/Drink.js');
+const DrinkVar = require('./models/DrinkVar.js');
+const DefaultParentDrink = require('./models/DefaultParentDrink.js');
 
 class ParentRealmController {
 	constructor() {
@@ -13,28 +16,66 @@ class ParentRealmController {
 		this.Account = Account;
 		this.Restaurant = Restaurant;
 		this.VoiceDevice = VoiceDevice;
+        this.Drink = Drink;
+        this.DrinkVar = DrinkVar;
+        this.DefaultParentDrink = DefaultParentDrink;
 
 		var that = this
 		this.realm = Realm.open({
 			path: './DataRealm/default.realm',
-			schema: [Order, OrderItem, Account, Restaurant, VoiceDevice],
+			schema: [Order, OrderItem, Account, Restaurant, VoiceDevice, Drink, DrinkVar, DefaultParentDrink],
 		}).then(realm => {
 			that.realm = realm;
 		})
 	};
 
+    formatRealmObj (objectElem) {
+        let result = null;
+        let worker = objectElem;
+        if (typeof worker === 'object') {
+            let toArray = false;
+            for (let key in worker) {
+                if (key === '0') {
+                    toArray = true;
+                    break;
+                }
+            }
+
+            if (toArray) {
+                result = [];
+                worker = Array.from(worker);
+                for (let x = 0; x < worker.length; x++) {
+                    result[x] = this.formatRealmObj(worker[x]);
+                }
+            } else if (JSON.stringify(worker) === JSON.stringify({})) {
+                result = [];
+            } else if (worker === null) {
+                result = false;
+            } else {
+                result = {};
+                for (let key in worker) {
+                    result[key] = this.formatRealmObj(worker[key]);
+                }
+            }
+        } else {
+            result = objectElem;
+        }
+        return result;
+    };
+
 	// Abstract methods
 	objectWithId(className, id) {
 		let object = this.realm.objects(className).filtered('id = $0', id);
-		if(object && object.length == 1) {
+		if(object && object.length === 1) {
+            object = this.formatRealmObj(object);
 			return object[0];
 		}
 		return;
 	};
 	objectsWithFilter(className, filter) {
 		let objects = this.realm.objects(className).filtered(filter);
-		let arrObjects = Array.from(objects)
-		return arrObjects;
+        objects = this.formatRealmObj(objects);
+		return objects;
 	};
 	createObject(className, objData) {
 		let object;
