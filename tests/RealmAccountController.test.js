9da0d6135be109ec9577ac2d11e0bfaccb64a2f7
Tobias Trinks <tobias.trinks@code.berlin>
2018-01-28 19:36:42 +0100

    updated tests after bug fixing

--- tests/RealmAccountController.test.js
+++ tests/RealmAccountController.test.js
@@ -1,111 +1,113 @@
-'use strict'
+'use strict';
 
 const RealmAccountController = require('../controller/RealmAccountController');
 const expect = require('chai').expect;
-const RealmAddressController = require('../controller/RealmAddressController')
+const RealmAddressController = require('../controller/RealmAddressController');
 
 var account;
 var address;
 
 describe('RealmAccountController', () => {
-	it('Create object and check methods', (done) => {
-		const controller = new RealmAccountController();
-		expect(controller.getAccount).to.be.a('Function')
-		expect(controller.createAccount).to.be.a('Function')
-		expect(controller.updateAccount).to.be.a('Function')
-		done();
-	})
-	describe('Valid Data', () => {
-		it('createAccount', (done) => {
-			const controller = new RealmAccountController();
-			controller.realm.then(realm => {
-				const accountMock = require('./mockData/account/createValid.json')
-				account = controller.createAccount(accountMock);
-				
-				expect(account).not.to.be.undefined;
-				expect(account.mail).to.equal(accountMock.mail);
-				expect(account.password).to.equal(accountMock.password);
-				expect(account.firstName).to.equal(accountMock.firstName);
-				expect(account.surname).to.equal(accountMock.surname);
-				expect(account.phone).to.equal(accountMock.phone);
+    it('Create object and check methods', (done) => {
+        const controller = new RealmAccountController();
+        expect(controller.getAccount).to.be.a('Function');
+        expect(controller.createAccount).to.be.a('Function');
+        expect(controller.updateAccount).to.be.a('Function');
+        done();
+    });
+    describe('Valid Data', () => {
+        it('createAccount', (done) => {
+            const controller = new RealmAccountController();
+            controller.realm.then(realm => {
+                const accountMock = require('./mockData/account/createValid.json');
+                account = controller.createAccount(accountMock);
 
-				expect(account.address).not.to.be.undefined;
-				expect(account.address.street).to.equal(accountMock.street);
-				expect(account.address.postCode).to.equal(accountMock.postCode);
-				expect(account.address.city).to.equal(accountMock.city);
-				expect(account.address.country).to.equal(accountMock.country);
-	
-				expect(account.created).to.be.an('date');
-				expect(account.id).to.be.an('string');
-				expect(account.id).not.to.be.empty;
-	
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-		it('updateAccount', (done) => {
-			const controller = new RealmAccountController();
-			controller.realm.then(realm => {
-				let newMail = "acbdefgadasd@de.as";
-				let newData = {
-					mail:newMail
-				}
-				let updatedAccount = controller.updateAccount(account.id, newData);
-				expect(updatedAccount.mail).to.equal(newMail);
-				expect(updatedAccount).to.deep.equal(account);
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-		it('getAccount', (done) => {
-			const controller = new RealmAccountController();
-			controller.realm.then(realm => {
-				let getAccount = controller.getAccount(account.id);
-				expect(getAccount).to.deep.equal(account);
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-	});
-	describe('Wrong Data', () => {
-		it('createAccount', (done) => {
-			const controller = new RealmAccountController();
-			controller.realm.then(realm => {
-				const accountMock = require('./mockData/account/createWrong.json')
-				let emptyAccount = controller.createAccount(accountMock);
-				expect(emptyAccount).to.be.undefined;
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-		it('updateAccount', (done) => {
-			const controller = new RealmAccountController();
-			controller.realm.then(realm => {
-				let newPostCode = "99999";
-				let newData = {
-					mail:[],
-					postCode:newPostCode
-				}
-				let updatedAccount = controller.updateAccount(account.id, newData);
-				expect(updatedAccount).to.be.undefined;
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-		it('getAccount', (done) => {
-			const controller = new RealmAccountController();
-			controller.realm.then(realm => {
-				let getAccount = controller.getAccount("xxxxxxsomerandomaccountid123456789");
-				expect(getAccount).to.be.undefined;
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-	});
-})
+                expect(account).not.to.be.undefined;
+                expect(account.mail).to.equal(accountMock.mail);
+                expect(account.password).to.equal(accountMock.password);
+                expect(account.firstName).to.equal(accountMock.firstName);
+                expect(account.surname).to.equal(accountMock.surname);
+                expect(account.phone).to.equal(accountMock.phone);
+
+                expect(account.address).not.to.be.undefined;
+                expect(account.address.street).to.equal(accountMock.street);
+                expect(account.address.postCode).to.equal(accountMock.postCode);
+                expect(account.address.city).to.equal(accountMock.city);
+                expect(account.address.country).to.equal(accountMock.country);
+
+                expect(account.created).to.be.an('date');
+                expect(account.id).to.be.an('string');
+                expect(account.id).not.to.be.empty;
+
+                done();
+            }).catch(err => {
+                done(err);
+            });
+        });
+        it('updateAccount', (done) => {
+            const controller = new RealmAccountController();
+            controller.realm.then(realm => {
+                let newMail = 'acbdefgadasd@de.as';
+                let newData = {
+                    mail: newMail
+                };
+                let updatedAccount = controller.updateAccount(account.id, newData);
+                expect(updatedAccount.mail).to.equal(newMail);
+                expect(updatedAccount).to.deep.equal(account);
+                done();
+            }).catch(err => {
+                done(err);
+            });
+        });
+        it('getAccount', (done) => {
+            const controller = new RealmAccountController();
+            controller.realm.then(realm => {
+                let getAccount = controller.getAccount(account.id);
+                getAccount = controller.formatRealmObj(getAccount);
+                account = controller.formatRealmObj(account);
+                expect(getAccount).to.deep.equal(account);
+                done();
+            }).catch(err => {
+                done(err);
+            });
+        });
+    });
+    describe('Wrong Data', () => {
+        it('createAccount', (done) => {
+            const controller = new RealmAccountController();
+            controller.realm.then(realm => {
+                const accountMock = require('./mockData/account/createWrong.json');
+                let emptyAccount = controller.createAccount(accountMock);
+                expect(emptyAccount).to.be.undefined;
+                done();
+            }).catch(err => {
+                done(err);
+            });
+        });
+        it('updateAccount', (done) => {
+            const controller = new RealmAccountController();
+            controller.realm.then(realm => {
+                let newPostCode = '99999';
+                let newData = {
+                    mail: [],
+                    postCode: newPostCode
+                };
+                let updatedAccount = controller.updateAccount(account.id, newData);
+                expect(updatedAccount).to.be.undefined;
+                done();
+            }).catch(err => {
+                done(err);
+            });
+        });
+        it('getAccount', (done) => {
+            const controller = new RealmAccountController();
+            controller.realm.then(realm => {
+                let getAccount = controller.getAccount('xxxxxxsomerandomaccountid123456789');
+                expect(getAccount).to.be.undefined;
+                done();
+            }).catch(err => {
+                done(err);
+            });
+        });
+    });
+});
--- tests/RealmRestaurantController.test.js
+++ tests/RealmRestaurantController.test.js
@@ -1,129 +1,142 @@
-'use strict'
-
 const RealmAccountController = require('../controller/RealmAccountController');
-const RealmRestaurantController = require('../controller/RealmRestaurantController')
-const expect = require('chai').expect
+const RealmRestaurantController = require('../controller/RealmRestaurantController');
+const chai = require('chai');
+const mocha = require('mocha');
 
-var restaurant;
-var account;
+const expect = chai.expect;
+const describe = mocha.describe;
+const it = mocha.it;
+let restaurant;
+let account;
 
 describe('RealmRestaurantController', () => {
-	it('Create object and check methods', () => {
-		const controller = new RealmRestaurantController();
-		expect(controller.getRestaurant).to.be.a('Function')
-		expect(controller.getRestaurants).to.be.a('Function')
-		expect(controller.createRestaurant).to.be.a('Function')
-		expect(controller.updateRestaurant).to.be.a('Function')
-	})
-	describe('Valid Data', () => {
-		it('createRestaurant', (done) => {
-			let accountController = new RealmAccountController();
-			accountController.realm.then(realm => {
-				account = accountController.createAccount(require('./mockData/account/createValid.json'))
-				let controller = new RealmRestaurantController();
-				controller.realm.then(realm => {
-					const mock = require('./mockData/restaurant/createValid.json');
-					mock.account = account;
-					mock.address = account.address;
-					restaurant = controller.createRestaurant(account.id, mock);
-					console.log(restaurant.drinkMenus);
-					expect(restaurant.name).to.equal(mock.name);
-					expect(restaurant.address).to.deep.equal(mock.address);
-					expect(restaurant.drinkMenus).to.equal(mock.drinkMenus);
-					expect(restaurant.account).to.deep.equal(account);
-					expect(restaurant.created).to.be.an('date');
-					expect(restaurant.id).to.be.an('string');
-					expect(restaurant.id).not.to.be.empty;
-					done();
-				}).catch(err => {
-					done(err);
-				})
-			}).catch(err => {
-				done(err);
-			});
-		})
-		it('updateRestaurant', (done) => {
-			const controller = new RealmRestaurantController();
-			controller.realm.then(realm => {
-				let newName = "rpiefBrxxYQn9HUzYUiKXqKwi0IsGR";
-				let updated = controller.updateRestaurant(restaurant.id, {name:newName});
-				expect(updated.name).to.equal(newName);
-				expect(updated).to.deep.equal(restaurant);
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-		it('getRestaurant', (done) => {
-			const controller = new RealmRestaurantController();
-			controller.realm.then(realm => {
-				let getRestaurant = controller.getRestaurant(restaurant.id);
-				expect(getRestaurant).to.deep.equal(restaurant);
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-		it('getRestaurants', (done) => {
-			const controller = new RealmRestaurantController();
-			controller.realm.then(realm => {
-				let restaurants = controller.getRestaurants(account.id);
-				expect(restaurants).to.have.lengthOf(1);
-				let first = restaurants[0];
-				expect(first).to.deep.equal(restaurant);
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-	});
-	describe('Wrong Data', () => {
-		it('createRestaurant', (done) => {
-			let controller = new RealmRestaurantController();
-			controller.realm.then(realm => {
-				const mock = require('./mockData/restaurant/createWrong.json')
-				let emptyRestaurant = controller.createRestaurant(account.id, mock);
-				expect(emptyRestaurant).to.be.undefined;
-				done();
-			}).catch(err => {
-				done(err);
-			})
-		})
-		it('updateRestaurant', (done) => {
-			const controller = new RealmRestaurantController();
-			controller.realm.then(realm => {
-				let id = "rpiefBrxxYQn9HUzYUiKXqKwi0IsGR";
-				const mock = require('./mockData/restaurant/createWrong.json')
-				let updated = controller.updateRestaurant(id, mock);
-				expect(updated).to.be.undefined;
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-		it('getRestaurant', (done) => {
-			const controller = new RealmRestaurantController();
-			controller.realm.then(realm => {
-				let id = "rpiefBrxxYQn9HUzYUiKXqKwi0IsGR";
-				let getRestaurant = controller.getRestaurant(id);
-				expect(getRestaurant).to.be.undefined;
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-		it('getRestaurants', (done) => {
-			const controller = new RealmRestaurantController();
-			controller.realm.then(realm => {
-				let id = "rpiefBrxxYQn9HUzYUiKXqKwi0IsGR";
-				let restaurants = controller.getRestaurants(id);
-				expect(restaurants).to.have.lengthOf(0);
-				let first = restaurants[0];
-				expect(first).to.be.undefined;
-				done();
-			}).catch(err => {
-				done(err);
-			});
-		})
-	});
-})
+    it('Create object and check methods', () => {
+        const controller = new RealmRestaurantController();
+        expect(controller.getRestaurant).to.be.a('Function');
+        expect(controller.getRestaurants).to.be.a('Function');
+        expect(controller.createRestaurant).to.be.a('Function');
+        expect(controller.updateRestaurant).to.be.a('Function');
+    });
+    describe('Valid Data', () => {
+        it('createRestaurant', (done) => {
+            const accountController = new RealmAccountController();
+            accountController.realm.then((realm) => {
+                account = accountController.createAccount(require('./mockData/account/createValid.json'));
+                const controller = new RealmRestaurantController();
+                controller.realm.then((realm) => {
+                    let mock = require('./mockData/restaurant/createValid.json');
+
+                    mock.account = account;
+                    mock.address = account.address;
+                    restaurant = controller.createRestaurant(account.id, mock);
+
+                    account = controller.formatRealmObj(account);
+                    restaurant = controller.formatRealmObj(restaurant);
+                    mock = controller.formatRealmObj(mock);
+
+                    expect(restaurant.name).to.equal(mock.name);
+                    expect(restaurant.address).to.deep.equal(mock.address);
+                    expect(restaurant.drinkMenus).to.deep.equal(mock.drinkMenus);
+                    expect(restaurant.account).to.deep.equal(account);
+                    expect(restaurant.created).to.be.an('date');
+                    expect(restaurant.id).to.be.an('string');
+                    expect(restaurant.id).not.to.be.empty;
+                    done();
+                }).catch((err) => {
+                    done(err);
+                });
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        it('updateRestaurant', (done) => {
+            const controller = new RealmRestaurantController();
+            controller.realm.then((realm) => {
+                const newName = 'rpiefBrxxYQn9HUzYUiKXqKwi0IsGR';
+                let updated = controller.updateRestaurant(restaurant.id, { name: newName });
+
+                restaurant.name = newName;
+                updated = controller.formatRealmObj(updated);
+
+                expect(updated.name).to.equal(newName);
+                expect(updated).to.deep.equal(restaurant);
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        it('getRestaurant', (done) => {
+            const controller = new RealmRestaurantController();
+            controller.realm.then((realm) => {
+                let getRestaurant = controller.getRestaurant(restaurant.id);
+                getRestaurant = controller.formatRealmObj(getRestaurant);
+                expect(getRestaurant).to.deep.equal(restaurant);
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        it('getRestaurants', (done) => {
+            const controller = new RealmRestaurantController();
+            controller.realm.then((realm) => {
+                let restaurants = controller.getRestaurants(account.id);
+                restaurants = controller.formatRealmObj(restaurants);
+                expect(restaurants).to.have.lengthOf(1);
+                const first = restaurants[0];
+                expect(first).to.deep.equal(restaurant);
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+    });
+    describe('Wrong Data', () => {
+        it('createRestaurant', (done) => {
+            const controller = new RealmRestaurantController();
+            controller.realm.then((realm) => {
+                const mock = require('./mockData/restaurant/createWrong.json');
+                const emptyRestaurant = controller.createRestaurant(account.id, mock);
+                expect(emptyRestaurant).to.be.undefined;
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        it('updateRestaurant', (done) => {
+            const controller = new RealmRestaurantController();
+            controller.realm.then((realm) => {
+                const id = 'rpiefBrxxYQn9HUzYUiKXqKwi0IsGR';
+                const mock = require('./mockData/restaurant/createWrong.json');
+                const updated = controller.updateRestaurant(id, mock);
+                expect(updated).to.be.undefined;
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        it('getRestaurant', (done) => {
+            const controller = new RealmRestaurantController();
+            controller.realm.then((realm) => {
+                const id = 'rpiefBrxxYQn9HUzYUiKXqKwi0IsGR';
+                const getRestaurant = controller.getRestaurant(id);
+                expect(getRestaurant).to.be.undefined;
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+        it('getRestaurants', (done) => {
+            const controller = new RealmRestaurantController();
+            controller.realm.then((realm) => {
+                const id = 'rpiefBrxxYQn9HUzYUiKXqKwi0IsGR';
+                const restaurants = controller.getRestaurants(id);
+                expect(restaurants).to.have.lengthOf(0);
+                const first = restaurants[0];
+                expect(first).to.be.undefined;
+                done();
+            }).catch((err) => {
+                done(err);
+            });
+        });
+    });
+});
